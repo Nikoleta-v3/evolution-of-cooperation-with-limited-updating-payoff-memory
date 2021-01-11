fd7dd30e8ecb5887936969412c52b9eed70bf1a5
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-01-11 11:38:51 +0100

    parallelize matlab code and make sure it can run for terminal

--- matlab/evolRun.m
+++ matlab/evolRun.m
@@ -4,8 +4,8 @@ starting_resident = [0, 0, 0];
 N = 100;
 delta = 0.999;
 beta = 1;
-numberIterations= 10 ^ 4;
-stochastic = 0;
+numberIterations= 10 ^ 7;
+stochastic = 1;
 
 n = 11;
 Ss = linspace(-2, 2, n);
@@ -21,11 +21,11 @@ for i=1:n
 end
 
 N = n * n;
-parfor (i = 1:N)
+parfor (i = 1:N, 8)
     S = payoffs(i, 1);
     T = payoffs(i, 2);
     u = [1, S, T, 0];
-    filename = "data/S_" + S + "_T_" + T;
+    filename = "data/S_" + S + "_T_" + T + "_stochastic_" + stochastic;
     evolSimulation(starting_resident, u, N, delta, beta, numberIterations, stochastic, filename);
 end
 end
