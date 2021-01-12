4e637c48f91351f520d255bd7914e4b1d537ff40
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-01-12 21:28:11 +0100

    run isort

--- src/evol_dynamics/__init__.py
+++ src/evol_dynamics/__init__.py
@@ -1,26 +1,12 @@
-from .evolution import (
-    fixation_probability_for_expected_payoffs,
-    fixation_probability_for_stochastic_payoffs,
-    imitation_probability,
-)
-from .formulation import (
-    expected_distribution_last_round,
-    expected_distribution_opening_round,
-    markov_chain_for_reactive_strategies,
-    steady_state,
-)
+from .evolution import (fixation_probability_for_expected_payoffs,
+                        fixation_probability_for_stochastic_payoffs,
+                        imitation_probability)
+from .formulation import (expected_distribution_last_round,
+                          expected_distribution_opening_round,
+                          markov_chain_for_reactive_strategies, steady_state)
 from .invasion import simulate_until_invasion
-from .numerical import (
-    donation_game,
-    harmony_game,
-    snowdrift_game,
-    stag_hunt_game,
-)
-from .simulation import (
-    ReactivePlayer,
-    get_opponents_of_mutant,
-    get_opponents_of_resident,
-    get_score_for_last_n_turns,
-    introduce_mutant,
-    simulation,
-)
+from .numerical import (donation_game, harmony_game, snowdrift_game,
+                        stag_hunt_game)
+from .simulation import (ReactivePlayer, get_opponents_of_mutant,
+                         get_opponents_of_resident, get_score_for_last_n_turns,
+                         introduce_mutant, simulation)
