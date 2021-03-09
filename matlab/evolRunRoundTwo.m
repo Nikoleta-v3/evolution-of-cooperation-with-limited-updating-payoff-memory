4f131ff152a25bbb560bcaa89f176c6eead1c26d
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-03-09 17:58:08 +0100

    fix run round two

--- matlab/evolRunRoundTwo.m
+++ matlab/evolRunRoundTwo.m
@@ -1,7 +1,8 @@
 function evolRunRoundTwo(S);
 
-
 num_workers = 11;
+my_pool = parpool(num_workers);
+
 starting_resident = [0, 0, 0];
 N = 100;
 delta = 0.999;
@@ -12,6 +13,7 @@ n=11;
 Ts=linspace(-1, 3, n);
 
 parfor i = 1:n
+    T=Ts(i);
     u = [1, S, T, 0];
     filename = "data/round_two/S_" + S + "_T_" + T;
     evolSimulationRoundTwo(starting_resident, u, N, delta, beta, numberIterations, filename);
