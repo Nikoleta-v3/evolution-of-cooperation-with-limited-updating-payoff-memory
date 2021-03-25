8e1328bbea443eff4a7938f3795e0be0f8339e20
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-03-25 10:45:21 +0100

    test case

--- matlab/evolRun.m
+++ matlab/evolRun.m
@@ -1,34 +1,39 @@
 function evolRun();
 
-num_workers = 16;
+num_workers = 1;
 my_pool = parpool(num_workers);
 
 starting_resident = [0, 0, 0];
 N = 100;
 delta = 0.999;
-beta = 10;
-numberIterations= 10 ^ 7;
+beta = 1;
+numberIterations= 10 ^ 8;
 stochastic = 0;
 
-n = 11;
-Ss = linspace(-2, 2, n);
-Ts = linspace(-1, 3, n);
+u = [0, -1, 1, 0];
 
-payoffs = zeros(n * n, 2);
+filename = "data/expected/donation_b_1";
+evolSimulation(starting_resident, u, N, delta, beta, numberIterations, stochastic, filename);
 
-for i=1:n
-    for j=1:n
-        payoffs(j + (i - 1) + (i -1) * 10, 1) = Ss(i);
-        payoffs(j + (i - 1) + (i -1) * 10, 2) = Ts(j);
-    end
-end
+% n = 11;
+% Ss = linspace(-2, 2, n);
+% Ts = linspace(-1, 3, n);
 
-lenght = n * n;
-parfor i = 1:lenght
-    S = payoffs(i, 1);
-    T = payoffs(i, 2);
-    u = [1, S, T, 0];
-    filename = "data/expected/S_" + S + "_T_" + T + "_beta_" + beta;
-    evolSimulation(starting_resident, u, N, delta, beta, numberIterations, stochastic, filename);
-end
-end
+% payoffs = zeros(n * n, 2);
+
+% for i=1:n
+%     for j=1:n
+%         payoffs(j + (i - 1) + (i -1) * 10, 1) = Ss(i);
+%         payoffs(j + (i - 1) + (i -1) * 10, 2) = Ts(j);
+%     end
+% end
+
+% lenght = n * n;
+% parfor i = 1:lenght
+%     S = payoffs(i, 1);
+%     T = payoffs(i, 2);
+%     u = [1, S, T, 0];
+%     filename = "data/expected/S_" + S + "_T_" + T + "_beta_" + beta;
+%     evolSimulation(starting_resident, u, N, delta, beta, numberIterations, stochastic, filename);
+% end
+% end
