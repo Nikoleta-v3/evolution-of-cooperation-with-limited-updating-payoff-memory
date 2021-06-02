355230b8a156e4c2d6cbcd9073c3693df6cde4d6
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-06-02 23:37:49 +0200

    moving calculation of fixation probability to files based on method

---
+++ matlab/expected.m
@@ -0,0 +1,14 @@
+function [phi]=expected(N, piMM, piMR, piRR, piRM);
+%% Calculates the fixation probability based on the expected payoffs
+
+laplus=zeros(1, N-1); laminus=laplus;
+for k=1:N-1
+    piM = (k-1) / (N-1) * piMM + (N-k) / (N-1) * piMR;
+    piR = k / (N-1) * piRM + (N-k-1) / (N-1) * piRR;
+
+    laplus(k) = 1 / (1 + exp(-beta * (piM - piR)));
+    laminus(k) = 1 /(1 + exp(-beta * (piR - piM)));
+end
+
+phi = 1 / (1 + sum(cumprod(laminus./laplus)));
+end
---
+++ matlab/last_round.m
@@ -0,0 +1,18 @@
+function [phi]=last_round(N, vRM, vMM, vMR, vRR, Rho);
+%% Calculates the fixation probability based on the last round payoffs
+
+laplus=zeros(1, N-1); laminus=laplus;
+for k=1:N-1
+    x=zeros(4,4);
+    for i1=1:4
+        for i2=1:4
+            x(i1,i2)=1/(N-1)*vRM(i1)*((i1==1 & i2==1) | (i1==2 & i2==3) | (i1==3 &i2==2) | (i1==4 & i2==4))...
+                + (1-1/(N-1))/(N-2)/(N-3) * ((k-1)*(k-2)*vRM(i1)*vMM(i2) + (k-1)*(N-k-1)*vRM(i1)*vMR(i2) + (N-k-1)*(k-1)*vRR(i1)*vMM(i2) + (N-k-1)*(N-k-2)*vRR(i1)*vMR(i2));
+        end
+    end
+    laplus(k)=sum(sum(x.*Rho));
+    laminus(k)=sum(sum(x.*Rho'));
+end
+
+phi = 1 / (1 + sum(cumprod(laminus./laplus)));
+end
