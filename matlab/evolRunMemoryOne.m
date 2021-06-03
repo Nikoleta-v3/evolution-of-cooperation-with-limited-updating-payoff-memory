f38f212fe2994d1ecf5b0243fe33934f02d23a5c
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-06-03 16:50:30 +0200

    script to run memory one experiment

---
+++ matlab/evolRunMemoryOne.m
@@ -0,0 +1,17 @@
+function evolRunMemoryOne();
+
+starting_resident = [0, 0, 0, 0, 0];
+N = 100;
+delta = 0.999;
+beta = 1;
+numberIterations = 10 ^ 7;
+payoff_types = ["expected", "last_round"]
+
+u = [2, -1, 3, 0];
+
+parfor (i = 1:2)
+    payoff_type = payoff_types(i);
+    filename = "../data/memory_one_" + payoff_type;
+    evolSimulation(starting_resident, u, N, delta, beta, numberIterations, payoff_type, filename);
+end
+end
