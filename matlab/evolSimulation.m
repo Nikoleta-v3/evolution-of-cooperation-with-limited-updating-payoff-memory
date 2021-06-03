930a9d722af48366b440aca1804f9dc969877971
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-06-03 11:46:08 +0200

    write with precision

--- matlab/evolSimulation.m
+++ matlab/evolSimulation.m
@@ -20,7 +20,7 @@ for t = progress(1:numberIterations)
     end
 end
 
-csvwrite(filename + ".csv", xDat);
+dlmwrite(filename + ".csv", xDat, 'precision', 9);
 writematrix(Data, filename + ".txt");
 
 AvCoop = mean(xDat(:,end-2));
