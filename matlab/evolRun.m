99904fb3721b59c8e95e742f3276badae17f7b52
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2021-06-04 18:47:22 +0200

    Restructure matlab code (#4)
    * moving calculation of fixation probability to files based on method
    
    * adds docstring to stationary
    
    * re write script to use fixation functions
    
    * close end
    
    * add semicolom
    
    * fix nested issue
    
    * debug code
    
    * run tests only on pull requests
    
    * includes two opponents type
    
    * prepare to run simulations (for 3 types)
    
    * add return statement
    
    * corrects return variable
    
    * rename matlab scripts for phi
    
    * rename rho to phi
    
    * write with precision
    
    * fix opponents probabilities
    
    * style
    
    * add seed
    
    * run benefits example for longer
    
    * restructure code for 16 states examples
    
    * remove extra run files
    
    * add memory one scripts
    
    * script to run memory one experiment
    
    * use correct function
    
    * rename script
    
    * prep to run sixteen states

--- matlab/evolRun.m
+++ matlab/evolRun.m
@@ -4,28 +4,14 @@ starting_resident = [0, 0, 0];
 N = 100;
 delta = 0.999;
 beta = 1;
-numberIterations= 10 ^ 7;
-stochastic = 1;
+numberIterations = 10 ^ 7;
+payoff_types = ["two_rounds", "two_rounds_opponents"]
 
-n = 11;
-Ss = linspace(-2, 2, n);
-Ts = linspace(-1, 3, n);
+u = [2, -1, 3, 0];
 
-payoffs = zeros(n * n, 2);
-
-for i=1:n
-    for j=1:n
-        payoffs(j + (i - 1) + (i -1) * 10, 1) = Ss(i);
-        payoffs(j + (i - 1) + (i -1) * 10, 2) = Ts(j);
-    end
-end
-
-N = n * n;
-parfor (i = 1:N, 8)
-    S = payoffs(i, 1);
-    T = payoffs(i, 2);
-    u = [1, S, T, 0];
-    filename = "data/stochastic/S_" + S + "_T_" + T + "_stochastic_" + stochastic;
-    evolSimulation(starting_resident, u, N, delta, beta, numberIterations, stochastic, filename);
+parfor (i = 1:2)
+    payoff_type = payoff_types(i);
+    filename = "../data/sixteen_states_" + payoff_type;
+    evolSimulationTwoRounds(starting_resident, u, N, delta, beta, numberIterations, payoff_type, filename);
 end
 end
---
+++ matlab/phiExpected.m
@@ -0,0 +1,14 @@
+function [phi]=phiExpected(N, piMM, piMR, piRR, piRM, beta);
+%% Calculates the fixation probability based on the expected payoffs
+
+laplus=zeros(1, N-1); laminus=laplus;
+for k=1:N-1
+    piM = (k-1) / (N-1) * piMM + (N-k) / (N-1) * piMR;
+    piR = k / (N-1) * piRM + (N-k-1) / (N-1) * piRR;
+
+    laplus(k) = 1 / (1 + exp(-beta * (piM - piR)));
+    laminus(k) = 1 / (1 + exp(-beta * (piR - piM)));
+end
+
+phi = 1 / (1 + sum(cumprod(laminus./laplus)));
+end
