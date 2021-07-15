e4b92ef7a9c009a195e8a8f822662c9e4bb72992
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-07-15 15:58:55 +0200

    run only for two opponents

--- matlab/evolRun.m
+++ matlab/evolRun.m
@@ -5,13 +5,13 @@ N = 100;
 delta = 0.999;
 beta = 1;
 numberIterations = 10 ^ 7;
-payoff_types = ["two_opponents",]
+% payoff_types = ["two_opponents"]
 
 u = [2, -1, 3, 0];
 
-parfor (i = 1:2)
-    payoff_type = payoff_types(i);
-    filename = "../data/" + payoff_type;
-    evolSimulationTwoRounds(starting_resident, u, N, delta, beta, numberIterations, payoff_type, filename);
-end
+% parfor (i = 1:2)
+payoff_type = "two_opponents";
+filename = "../data/" + payoff_type;
+evolSimulationTwoRounds(starting_resident, u, N, delta, beta, numberIterations, payoff_type, filename);
+% end
 end
