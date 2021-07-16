ebbd2be20410343687306407f862a0e261f896ab
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-07-17 00:57:52 +0200

    payoffs run for mem one

--- matlab/evolRunMemoryOne.m
+++ matlab/evolRunMemoryOne.m
@@ -3,15 +3,23 @@ function evolRunMemoryOne();
 starting_resident = [0, 0, 0, 0, 0];
 N = 100;
 delta = 0.999;
-betas = [10 ^ - 2, 10 ^ -1, 10 ^ 0, 10 ^ 1, 10 ^ 2];
+beta = 
 numberIterations = 10 ^ 7;
 payoff_type = "expected" % "last_round"
 
-u = [2, -1, 3, 0];
+us = [1, -1, 2, 0;
+      2, -1, 3, 0;
+      3, -1, 4, 0;
+      4, -1, 5, 0;
+      5, -1, 6, 0;
+      6, -1, 7, 0;
+      7, -1, 8, 0;
+      8, -1, 9, 0;
+      9, -1, 10, 0];
 
-parfor (i = 1:5)
-    beta = betas(i);
-    filename = "../data/memory_one_" + payoff_type + "_beta_" + beta;
+parfor (i = 1:9)
+    u = us(i, :);
+    filename = "../data/memory_one_" + payoff_type + "_b_" + u(3);
     evolSimulationMemoryOne(starting_resident, u, N, delta, beta, numberIterations, payoff_type, filename);
 end
 end
