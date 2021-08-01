a0798db3c2596589c27fec8e3cd86c2916c771d4
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-08-01 14:19:09 +0200

    prep to run expected experiments

--- matlab/RunMutation.m
+++ matlab/RunMutation.m
@@ -4,15 +4,15 @@ starting_resident = [0, 0, 0];
 N = 100;
 delta = 0.999;
 beta = 1;
-mutation = 0.01;
-numberIterations = 10 ^ 7;
-payoff_types = ["expected", "last_round"]
+mutations = [10 ^ -4, 10 ^ -3, 10 ^ -2, 10 ^ -1, 10 ^ 0];
+numberIterations = 10 ^ 9;
+payoff_type = "expected"; %, "last_round"]
 
 u = [2, -1, 3, 0];
 
-parfor (i = 1:2)
-payoff_type = payoff_types(i);
-filename = "../data/mutation_" + payoff_type;
+parfor (i = 1:5)
+mutation = mutations(i);
+filename = "../data/mutation_" + payoff_type + "_mutation_" + mutation;
 evolMutation(starting_resident, u, N, delta, beta, mutation, numberIterations, payoff_type, filename);
 end
 end
