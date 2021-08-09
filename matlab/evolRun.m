ec7e30910f4b89ffb247059afee20af7acc80e57
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-08-09 12:11:23 +0200

    run more memory experiments for b = 10

--- matlab/evolRun.m
+++ matlab/evolRun.m
@@ -4,17 +4,18 @@ starting_resident = [0, 0, 0];
 N = 100;
 delta = 0.999;
 beta = 1;
-numberIterations = 10 ^ 6;
-payoff_type = "two_rounds_opponents";
+numberIterations = 10 ^ 7;
+payoff_types = ["two_rounds", "two_opponents"];
 
 
-u = [2, -1, 3, 0];
+u = [9, -1, 10, 0];
 
-% parfor (i = 1:2)
+parfor (i = 1:2)
 % numberIterations = numbersIterations(i);
-seed = 1;
-rng(seed);
-filename = "../data/" + payoff_type + "_iterations_" + numberIterations;
+payoff_type = payoff_types(i)
+% seed = 1;
+% rng(seed);
+filename = "../data/" + payoff_type + "_b_10";
 evolSimulationTwoRoundsOpponents(starting_resident, u, N, delta, beta, numberIterations, payoff_type, filename);
-% end
+end
 end
