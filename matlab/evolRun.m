659d2c22cac7c0c912e9c13e29be354b1b83ea3c
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-11-16 13:04:01 +0100

    run 2x2 games plots

--- matlab/evolRun.m
+++ matlab/evolRun.m
@@ -4,16 +4,13 @@ starting_resident = [0, 0, 0];
 N = 100;
 delta = 0.999;
 beta = 1;
-numberIterations = 10 ^ 6;
-payoff_type = "two_rounds_opponents";
-u = [9, -1, 10, 0];
+numberIterations = 10 ^ 7;
+payoff_type = "expected";
+payoffs = readmatrix("two_by_two_games_values.csv");
 
-seeds = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-parfor (i = 1:9)
-    seed = seeds(i);
-    rng(seed);
-    filename = "../data/" + payoff_type + "_seed_" + seed;
-    evolSimulationTwoRoundsOpponents(starting_resident, u, N, delta, beta, numberIterations, payoff_type, filename);
+parfor (i = 1:11)
+    u = payoffs(i, :);
+    filename = "../data/two_by_two_games_values_S" + u(2) + "_T_" + u(2);
+    evolSimulation(starting_resident, u, N, delta, beta, numberIterations, payoff_type, filename);
 end
 end
