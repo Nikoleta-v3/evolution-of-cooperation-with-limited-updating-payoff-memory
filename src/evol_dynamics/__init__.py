f158f95438f247a1962c8281009de7e921972940
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2022-08-16 10:51:39 +0200

    committing phase two

--- src/evol_dynamics/__init__.py
+++ src/evol_dynamics/__init__.py
@@ -1,27 +1,27 @@
-# from .evolution import (
-#     fixation_probability_for_expected_payoffs,
-#     fixation_probability_for_stochastic_payoffs,
-#     imitation_probability,
-# )
+from .evolution import (
+    fixation_probability_for_expected_payoffs,
+    fixation_probability_for_stochastic_payoffs,
+    imitation_probability,
+)
 from .formulation import (
     expected_distribution_last_round,
     expected_distribution_opening_round,
     markov_chain_for_reactive_strategies,
     steady_state,
 )
-# from .invasion import simulate_until_invasion
-# from .numerical import (
-#     donation_game,
-#     harmony_game,
-#     snowdrift_game,
-#     stag_hunt_game,
-# )
-# from .simulation import (
-#     ReactivePlayer,
-#     create_population,
-#     match_pairs,
-#     stationary_for_16_states,
-#     simulated_states,
-#     get_probabilities_for_opponents,
-#     theoretical_probabilities_for_opponents,
-# )
+from .invasion import simulate_until_invasion
+from .numerical import (
+    donation_game,
+    harmony_game,
+    snowdrift_game,
+    stag_hunt_game,
+)
+from .simulation import (
+    ReactivePlayer,
+    create_population,
+    match_pairs,
+    stationary_for_16_states,
+    simulated_states,
+    get_probabilities_for_opponents,
+    theoretical_probabilities_for_opponents,
+)
