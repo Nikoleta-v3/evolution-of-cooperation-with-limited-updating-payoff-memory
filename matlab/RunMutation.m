9220c6a56b2ea5a778abb1115ea27aae96d12cb5
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2022-09-06 11:35:59 +0200

    mutation code more efficient

--- matlab/RunMutation.m
+++ matlab/RunMutation.m
@@ -2,26 +2,40 @@ function RunMutation();
 
 starting_resident = [0, 0, 0];
 N = 100;
+sdim = 3;
 delta = 0.999;
 beta = 1;
 numberIterations = 10 ^ 5;
 
-data = ["expected", 10 ^ -1;
-        "last_round", 10 ^ -1];
+data = ["expected", 10 ^ -4;
+        "expected", 10 ^ -3;
+        "expected", 10 ^ -2;
+        "expected", 10 ^ -1;
+        "expected", 10 ^ 0;
+        "last_round", 10 ^ -4;
+        "last_round", 10 ^ -3;
+        "last_round", 10 ^ -2;
+        "last_round", 10 ^ -1;
+        "last_round", 10 ^ -0];
 
 u = [2, -1, 3, 0];
+n_run = 1;
 
-mutation = 10 ^ -1;
-payoff_type = "last_round";
+parpool();
+parfor (i = 1:10)
+        mutation = str2num(data(i, 2));
+        payoff_type = data(i, 1);
 
-filename = "../data/mutation_" + payoff_type + "_mutation_" + mutation;
-evolMutation(starting_resident, u, N, delta, beta, mutation, numberIterations, payoff_type, filename);
-% parpool();
-% parfor (i = 1:2)
-% mutation = str2num(data(i, 2));
-% payoff_type = data(i, 1);
-% 
-% filename = "../data/mutation_" + payoff_type + "_mutation_" + mutation;
-% evolMutation(starting_resident, u, N, delta, beta, mutation, numberIterations, payoff_type, filename);
-% end
+        filename = "../data/mutation_" + payoff_type + "_mutation_" + mutation + "_run_" + run;
+        population_fln = "../data/mutation_" + payoff_type + "_mutation_" + mutation + "_run_" + run - 1;
+        if n_run == 1;
+            %% Initialization
+            Res=starting_resident; 
+            population = Res .* ones(N, sdim, 'single');
+
+        else
+            population = readmatrix(population_fln + "population.csv");
+
+        end
+        evolMutation(population, u, N, delta, beta, mutation, numberIterations, payoff_type, filename);
 end
--- matlab/evolMutation.m
+++ matlab/evolMutation.m
@@ -1,23 +1,15 @@
-function [population, avg_player, coop]=evolMutation(starting_resident, u, N, delta, beta, mutation, numberIterations, payoff_type, filename);
+function [population, avg_player, coop]=evolMutation(population, u, N, delta, beta, mutation, numberIterations, payoff_type, filename);
 tic
 
-%% Initialization
-Res=starting_resident; sdim=3; avg_player=0; coop=0;
-
-population = Res .* ones(N, sdim, 'single');
-% xDat=zeros(numberIterations, 5);
-% xDat(1,:)=[0, Res, 0];
-% j = 1;
-
+sdim=3; avg_player=0; coop=0;
 %% Running the evolutionary process
 for t = progress(1:numberIterations)
-    if rand(1)<mutation
+    if rand(1) < mutation
         idx = randi(N);
-%         population = population(idx, :);
         population(idx, :) = rand(1, sdim);
     else
         indices = randi(N, [1, 2]);
-        
+
         if population(indices(1),:) ~= population(indices(2),:)
             if payoff_type == "expected"
                 payoffs = payoffsExpected(indices, population, u, N, delta);
@@ -32,7 +24,7 @@ for t = progress(1:numberIterations)
             end
 
             fermi = 1 / (1 + exp(-beta * (mutant_payoff - resident_payoff)));
-            if rand(1)<fermi
+            if rand(1) < fermi
                 population(indices(1), :) = population(indices(2), :);
             end
         end
@@ -43,13 +35,8 @@ for t = progress(1:numberIterations)
   sz = size(h, 1);
   avg_player = avg_player + sum(Mu .* h / sum(h), 1);
   coop = coop + cooperation(Mu, h, sz, delta);
-  
-%   if mod(t, 100000) == 0 
-%    xDat(j, :) = [t, avg_player, coop];
-%    j = j + 1;
-%   end    
+
 end
-% dlmwrite(filename + ".csv", xDat, 'precision', 9);
 
 Data=["R", "S", "T", "P", "N", "beta", "mutation", "numberIterations", "coop", "y", "p", "q";
       u(1),...
@@ -60,11 +47,11 @@ Data=["R", "S", "T", "P", "N", "beta", "mutation", "numberIterations", "coop", "
       beta,...
       mutation,...
       numberIterations,...
-      coop / numberIterations,...
-      avg_player / numberIterations];
+      coop,...
+      avg_player];
 
 writematrix(Data, filename + ".csv");
-writematrix(popultion, filename + "population_.csv");
+writematrix(population, filename + "population.csv");
 toc
 end
 
